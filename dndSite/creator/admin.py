caf473e5f020668b1cab0b036108366d42003d73
Caleb Ricketts <calebr@zillowgroup.com>
2018-07-02 12:20:28 -0500

    more creation w/ dndSite

--- dndSite/creator/admin.py
+++ dndSite/creator/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Character
+
 # Register your models here.
+
+admin.site.register(Character)
--- dndSite/creator/models.py
+++ dndSite/creator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Character(models.Model):
+    chr_name = models.CharField(max_length=50)
+    level = models.IntegerField(default=1)
+    player_name = models.CharField(max_length=50)
+
+    def __str(self):
+        return self.chr_name
